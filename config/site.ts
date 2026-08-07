7495f971b368000bd62c39da9e479cc383119065
kamal-2006 <kamaleshkamales006@gmail.com>
2026-08-07 22:24:04 +0530

    changed logo

--- config/site.ts
+++ config/site.ts
@@ -2,7 +2,7 @@ export const siteConfig = {
   name: "SafeGraph AI",
   description: "Build beautiful websites",
   navItems: [
-    { label: "Home", targetId: "home" },
+    { label: "Home", targetId: "home" },+
     { label: "About", targetId: "about" },
     { label: "Skills", targetId: "skills" },
     { label: "Projects", targetId: "projects" },
